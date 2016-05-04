ae2817b2d83be77bd67ca044a4f14887e5a1ed5d
Tanay Wakhare <twakhare@gmail.com>
2016-05-04 18:56:47 -0400

    multibyte XOR decryptor
    correctly solves Vigenere cipher by finding possible key sizes, then
    finding most probable key corresponding to each size. need to improve
    problem 3 plaintext scoring since that gives one incorrect key out of 29

--- Set 1/repeatingXORdecrypt_6.py
+++ Set 1/repeatingXORdecrypt_6.py
@@ -17,6 +17,19 @@ def HammingDistance(str1, str2):
                 count += 1
     return count
 
+#takes two bytearrrays
+def HammingB(arr1, arr2):
+    if len(arr1)!=len(arr2):
+        raise Exception("Differing array lengths")
+    count = 0
+    for i in xrange(len(arr1)):
+        bin1 = bin(arr1[i])[2:].zfill(8)
+        bin2 = bin(arr2[i])[2:].zfill(8)
+        for j in xrange(8):
+            if bin1[j]!=bin2[j]:
+                count += 1
+    return count
+
 #takes hex string
 #returns N best possiblities
 def findNkeysizes(str1,N):
@@ -31,28 +44,10 @@ def findNkeysizes(str1,N):
             arr4 = arr[3*size:4*size]
         except:
             print "index out of bounds - key is too long for the message"
-        count = 0.
         #average the normalized hamming distances btw 4 blocks of guessed key size
-        for i in xrange(len(arr1)):
-            bin1 = bin(arr1[i])[2:].zfill(8)
-            bin2 = bin(arr2[i])[2:].zfill(8)
-            bin3 = bin(arr3[i])[2:].zfill(8)
-            bin4 = bin(arr4[i])[2:].zfill(8)            
-            for j in xrange(8):
-                if bin1[j]!=bin2[j]:
-                    count += 1
-                if bin1[j]!=bin3[j]:
-                    count += 1
-                if bin1[j]!=bin4[j]:
-                    count += 1
-                if bin2[j]!=bin3[j]:
-                    count += 1
-                if bin2[j]!=bin4[j]:
-                    count += 1
-                if bin3[j]!=bin4[j]:
-                    count += 1
-                #TODO use hamming methods and hexlify/unhexlify to format it
-        distances.append([size,count/size])
+        count = HammingB(arr1,arr2)+HammingB(arr1,arr3)+HammingB(arr1,arr4)
+        count += HammingB(arr2,arr3)+HammingB(arr2,arr4)+HammingB(arr3,arr4)
+        distances.append([size,float(count)/size])
     distances.sort(key = itemgetter(1))
     return [item[0] for item in distances[:N]]
 
@@ -73,22 +68,34 @@ def breaktext(str1,size):
     substrings = [str1[i:i+2] for i in range(0, len(str1), 2)]
     blocks = [substrings[i::size] for i in range(size)]
     blocks = ["".join(block) for block in blocks]
-    print xor.printNtranslations(blocks[0],1)#some blocks still get no translations
-    #--> TODO check the single key decrypt method
-    key = [xor.printNtranslations(block,1)[0][1][2:].zfill(2) for block in blocks]
+    try:
+        key = [xor.printNtranslations(block,1)[0][1][2:].zfill(2) for block in blocks]
+    except:
+        return
     return "0x"+"".join(key)
 
 #takes text to break, number of possible decryptions to return
 def break_cipher(str1, N):
     sizes = findNkeysizes(str1, N)
-    print sizes
     keys = [breaktext(str1, size) for size in sizes]
+    keys = [key for key in keys if key!=None]
     encodings = [encode_repeating_xor(str1,key) for key in keys]
     return zip(*[encodings, keys])
 
-print HammingDistance("this is a test","wokka wokka!!!")#should be 37
+#return [ascii decrypted text, hex key]
+def best_guess(str1):
+    vals = break_cipher(str1,1)[0]
+    key = bytearray(vals[1][2:].decode("hex"))
+    text = bytearray(vals[0][2:].decode("hex"))
+    for i in xrange(len(text)):
+        text[i] ^= key[i%(len(vals[1])/2-1)]
+    return [str(text), vals[1]]
+
 file = open("challenge6.txt","r").read()
 text = "".join([line for line in file.splitlines()])
-vals = break_cipher(binascii.hexlify(base64.b64decode(text)),5)
-for v in vals:
-    print v
+text = binascii.hexlify(base64.b64decode(text))
+print binascii.unhexlify(best_guess(text)[1][2:])
+
+#problem_string = "4e07644e1a0f14004e4e1a1d1701074e490d4e190b0a4e4e640c1b010b4e0227000f3d3a1c1c000d1c024e1e0f6427640217001e4e2d001a0614070707000a491c1a1a4e4e08371d0b0b011a0a1c640b4e061742091d1a1d170a171d06063e0b4e0305"
+#print chr(int(xor.printNtranslations(test2,2)[1][1],16))
+#TODO print decrypted file
